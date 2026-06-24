140bc4e18e8de6b774bc6a9857815fe10ddb65f3
gemdrytech-stack <gemdrytech@gmail.com>
2026-06-24 12:45:01 +0530

    Update History.js

--- dryer_price_calculator/src/components/History.js
+++ dryer_price_calculator/src/components/History.js
@@ -7,7 +7,7 @@ export default function History() {
 
   const fetchQuotes = async () => {
     const res = await fetch(
-      `http://localhost:5000/api/offer/history?search=${search}`
+      `https://gem-drytech-offer-generator.onrender.com/api/offer/history?search=${search}`
     );
     const data = await res.json();
     setQuotes(data);
@@ -19,7 +19,7 @@ export default function History() {
 
   const downloadPDF = async (quoteNo) => {
     const res = await fetch(
-      `http://localhost:5000/api/offer/pdf/${quoteNo}`
+      `https://gem-drytech-offer-generator.onrender.com/api/offer/pdf/${quoteNo}`
     );
 
     const blob = await res.blob();
